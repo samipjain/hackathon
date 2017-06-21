6568a34b58383fd5cd46735b3b68d0abba6b15cb
samipjain <samipjain1216@gmail.com>
2017-06-21 17:43:29 +0530

    Home updated

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -9,8 +9,26 @@ angular.module('webApp.welcome', ['ngRoute', 'firebase'])
 	});
 }])
 
-.controller('WelcomeCtrl', ['$scope', '$firebaseAuth', 'CommonProp', function($scope, $firebaseAuth, CommonProp){
+.controller('WelcomeCtrl', ['$scope', '$firebaseAuth', 'CommonProp', '$firebaseObject', function($scope, $firebaseAuth, CommonProp, $firebaseObject){
 	
+	var getHappyPost;
 	$scope.username = CommonProp.getUser();
+
+	getHappyPost = function() {
+		var category = 'Happy';
+		var id = Math.floor((Math.random() * 10) + 1);
+		var ref = firebase.database().ref().child('Articles').child('Happy');
+		var obj = $firebaseObject(ref);
+		obj.$loaded().then(function(data){
+			console.log(data);
+			console.log(data[3]);
+			$scope.post = data;			
+		})
+		.catch(function(error){
+			console.log("Error: ", error);
+		})
+	}
+
+	getHappyPost();
 
 }]);
